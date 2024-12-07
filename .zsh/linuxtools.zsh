67eaf9bfb3202de604301d28f02fd733f436e208
Priyansh Khandal <160209363+k-priyansh@users.noreply.github.com>
2024-12-07 16:01:31 +0530

    Delete .zsh directory

--- .zsh/linuxtools.zsh
+++
@@ -1,7 +0,0 @@
-# -----------------------------------------------------
-# BugBounty Tools
-# -----------------------------------------------------
-alias pdtm="$HOME/tools/pdtm/pdtm"                                                  # pdtm command
-alias httpx="$HOME/tools/httpx/httpx"                                               # httpx command
-alias aquatone="$HOME/.tools/aquatone/aquatone"                                     # aquatone command
-
